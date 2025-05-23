1c16fc287302f57d15d29e9dd7b6a246b8ff81e4
Yash Vasani <147640478+yash-vasani-01@users.noreply.github.com>
2025-05-23 23:25:29 +0530

    Update assignment_1.py

--- Assignments/Assignment 1/assignment_1.py
+++ Assignments/Assignment 1/assignment_1.py
@@ -15,11 +15,21 @@ def print_pattern2(n):
         
         print()
         
+def print_pattern3(n):
+    for i in range(n):
+        for j in range(n-i+1,1,-1):
+            print('* ',end='')
+        print()
+        
             
             
 n=int(input('Enter a No of Row:'))
 print('Pattern 1')
 print_pattern1(n)
 
-print('pattern 2')
+print('Pattern 2')
+print()
+print_pattern3(n)
+
+print('pattern 3')
 print_pattern2(n)
